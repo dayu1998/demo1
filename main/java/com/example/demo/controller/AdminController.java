03d963b7bc65471458d9ab78480479a2dace354e
yuchenliu <yliu687@wisc.edu>
2021-12-06 14:40:41 -0500

    test

---
+++ main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import io.swagger.models.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.locks.ReentrantLock;
+
+@RestController
+@RequestMapping(value = "/admin")
+
+public class AdminController {
+
+}
